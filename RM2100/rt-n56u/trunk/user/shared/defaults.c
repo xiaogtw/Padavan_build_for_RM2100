1805b7d614b3c877821971a7804bbab72bb20fa6
xiaogtw <83878118+xiaogtw@users.noreply.github.com>
2021-05-13 20:43:20 +0800

    Update defaults.c

--- RM2100/rt-n56u/trunk/user/shared/defaults.c
+++ RM2100/rt-n56u/trunk/user/shared/defaults.c
@@ -68,10 +68,10 @@ struct nvram_pair router_defaults[] = {
 	{ "wan_ipaddr", "0.0.0.0" },		/* WAN IP address */
 	{ "wan_netmask", "0.0.0.0" },		/* WAN netmask */
 	{ "wan_gateway", "0.0.0.0" },		/* WAN gateway */
-	{ "wan_dnsenable_x", "1" },
-	{ "wan_dns1_x", "" },
-	{ "wan_dns2_x", "" },
-	{ "wan_dns3_x", "" },
+	{ "wan_dnsenable_x", "0" },
+	{ "wan_dns1_x", "114.114.114.114" },
+	{ "wan_dns2_x", "114.114.115.115" },
+	{ "wan_dns3_x", "8.8.8.8" },
 	{ "wan_hostname", "" },			/* WAN hostname */
 	{ "wan_vci", "" },			/* WAN vendor class identifier (OPT-60) */
 	{ "wan_ttl_fix", "0" },
